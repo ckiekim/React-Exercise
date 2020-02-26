fe8a43af249f1e394552b54f12e89848e39e1099
Cheolkie Kim <ckiekim@naver.com>
2020-02-26 17:40:48 +0900

    CRUD

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import Subject from './Components/Subject';
 import TOC from './Components/TOC';
-import Content from './Components/Content';
+import Control from './Components/Control';
+import ReadContent from './Components/ReadContent';
+import CreateContent from './Components/CreateContent';
+import UpdateContent from './Components/UpdateContent';
 import './App.css';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
+		this.max_content_id = 3;
 		this.state = {
-			mode: 'read',
-			selected_content_id: 2,
+			mode: 'welcome',
+			selected_content_id: 1,
 			subject: {title:'WEB', sub:'World Wide Web!'},
 			welcome: {title:'Welcome', desc:'Hello, React!!!'},
 			contents: [
@@ -19,21 +23,62 @@ class App extends Component {
 			]
 		}
 	}
-	render() {
-		var _title, _desc;
+	getItem() {
+		for (const data of this.state.contents) {
+			if (data.id === this.state.selected_content_id) {
+				//console.log(data);
+				return data;
+			}
+		}
+	}
+	getContent() {
+		var _data, _title, _desc, _article;
 		if (this.state.mode === 'welcome') {
 			_title = this.state.welcome.title;
 			_desc = this.state.welcome.desc;
-		} else {
-			for (let i=0; i<this.state.contents.length; i++) {
-				var data = this.state.contents[i];
-				if (data.id === this.state.selected_content_id) {
-					_title = data.title;
-					_desc = data.desc;
-					break;
+			_article = <ReadContent title={_title} desc={_desc}></ReadContent>
+		} else if (this.state.mode === 'read') {
+			_data = this.getItem();
+			_article = <ReadContent 
+							title={_data.title} desc={_data.desc}
+							onRead={function() {
+								this.setState({
+									selected_content_id: _data.id
+								});
+							}.bind(this)}
+						></ReadContent>
+		} else if (this.state.mode === 'create') {
+			_article = <CreateContent onCreateItem={function(title, desc) {
+				this.max_content_id++;
+				let _contents = this.state.contents.concat(			// 복제해서 실행시켜야 성능 향상 가능
+					{id:this.max_content_id, title:title, desc:desc}
+				);
+				this.setState({
+					mode: 'read',
+					selected_content_id: this.max_content_id,
+					contents: _contents
+				});
+			}.bind(this)}></CreateContent>
+		} else if (this.state.mode === 'update') {
+			_data = this.getItem();
+			_article = <UpdateContent data={_data} onUpdateItem={function(id, title, desc) {
+				let _contents = Array.from(this.state.contents);	// 복제해서 실행시켜야 성능 향상 가능
+				for (const item of _contents) {
+					if (item.id === id) {
+						item.title = title;
+						item.desc = desc;
+					}
 				}
-			}
+				this.setState({
+					mode: 'read',
+					selected_content_id: id,
+					contents: _contents
+				});
+			}.bind(this)}></UpdateContent>
 		}
+		return _article;
+	}
+	render() {
 		return (
 			<div className="App">
 				<Subject 
@@ -54,7 +99,32 @@ class App extends Component {
 					data={this.state.contents}
 				></TOC>
 				<br></br>
-				<Content title={_title} desc={_desc}></Content>
+				<Control 
+					onChangeMode={function(_mode) {
+						if (_mode === 'delete') {
+							if (window.confirm('Confirm message')) {
+								let _contents = Array.from(this.state.contents);
+								let index = 0;
+								for (const item of _contents) {
+									if (item.id === this.state.selected_content_id) {
+										_contents.splice(index, 1);
+										break;
+									}
+									index++;
+								}
+								this.setState({
+									mode: 'welcome',
+									contents: _contents
+								});
+								alert('Deleted.')
+							}
+						} else {
+							this.setState({mode: _mode});
+						}
+					}.bind(this)}
+				></Control>
+				<hr></hr>
+				{this.getContent()}
 			</div>
 		);
 	}
