4293c4f0a11bd2d926c40cf61502c6415d415ce9
Cheolkie Kim <ckiekim@naver.com>
2020-02-26 09:18:08 +0900

    Components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,63 @@
 import React, { Component } from 'react';
+import Subject from './Components/Subject';
+import TOC from './Components/TOC';
+import Content from './Components/Content';
 import './App.css';
 
-class Subject extends Component {
-  render() {
-    return (
-      <header>
-        <h1>{this.props.title}</h1>
-        {this.props.sub}
-      </header>
-    );
-  }
-}
-class TOC extends Component {
-  render() {
-    return (
-      <nav>
-        <ul>
-          <li><a href="1.html">HTML</a></li>
-          <li><a href="2.html">CSS</a></li>
-          <li><a href="3.html">JavaScript</a></li>
-        </ul>
-      </nav>
-    );
-  }
-}
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-  }
-}
-
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Subject title="WEB" sub="World Wide Web!"></Subject>
-        <Subject title="React" sub="For UI"></Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			mode: 'read',
+			selected_content_id: 2,
+			subject: {title:'WEB', sub:'World Wide Web!'},
+			welcome: {title:'Welcome', desc:'Hello, React!!!'},
+			contents: [
+				{id:1, title:'HTML', desc:'HTML is HyperText ...'},
+				{id:2, title:'CSS', desc:'CSS is Cascading ...'},
+				{id:3, title:'JavaScript', desc:'JS is JavaScript ...'}
+			]
+		}
+	}
+	render() {
+		var _title, _desc;
+		if (this.state.mode === 'welcome') {
+			_title = this.state.welcome.title;
+			_desc = this.state.welcome.desc;
+		} else {
+			for (let i=0; i<this.state.contents.length; i++) {
+				var data = this.state.contents[i];
+				if (data.id === this.state.selected_content_id) {
+					_title = data.title;
+					_desc = data.desc;
+					break;
+				}
+			}
+		}
+		return (
+			<div className="App">
+				<Subject 
+					title={this.state.subject.title} 
+					sub={this.state.subject.sub}
+					onChangePage={function() {
+						this.setState({mode: 'welcome'});
+					}.bind(this)}
+				></Subject>
+				<br></br>
+				<TOC
+					onChangePage={function(id) {
+						this.setState({
+							mode: 'read',
+							selected_content_id: parseInt(id)
+						});
+					}.bind(this)} 
+					data={this.state.contents}
+				></TOC>
+				<br></br>
+				<Content title={_title} desc={_desc}></Content>
+			</div>
+		);
+	}
 }
 
 export default App;
